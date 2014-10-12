1d43d9e91a441bb1bd2143553e2419505f5df826
longpham.uit <longpham.uit@gmail.com>
2014-10-12 17:52:19 +0000

    + Motobike Animation

---
+++ Assets/Script/ScooterHandler.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum MyDirection {
+	left,
+	right,
+	ahead
+}
+
+public class ScooterHandler : MonoBehaviour {
+
+	private Animator anim;
+
+	MyDirection dir = MyDirection.ahead;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.A) == true) { //left
+			if (dir != MyDirection.left) {
+				dir = MyDirection.left;
+				anim.SetInteger ("dir",2);
+			}
+		} else if (Input.GetKey (KeyCode.D) == true) { //left
+			if (dir != MyDirection.right) {
+				dir = MyDirection.right;
+				anim.SetInteger ("dir",1);
+			}
+		} else {
+			dir = MyDirection.ahead;
+			anim.SetInteger ("dir",0);
+		}
+	}
+}
