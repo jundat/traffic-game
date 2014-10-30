989fe9eb7bc364cbfff771b85b418ecca4eb595b
longpham.uit@gmail.com <longpham.uit@gmail.com@f1fda995-d3ca-aaca-87fc-4b9f00a53c2b>
2014-10-30 19:46:13 +0000

    + Traffic Lights OK!

---
+++ trunk/Assets/Script/Handler/TrafficLightHandler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrafficLightHandler : MonoBehaviour {
+
+	public Material red;
+	public Material yellow;
+	public Material green;
+	public Material black;
+
+	void Start () {}
+
+	void Update () {}
+
+
+}
