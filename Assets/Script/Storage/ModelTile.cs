2e15221c20ee09f983cad2fa88458cc35daa8386
longpham.uit <longpham.uit@gmail.com>
2014-10-30 16:11:09 +0000

    + Skybox + Sign Direction

--- Assets/Script/Storage/ModelTile.cs
+++ Assets/Script/Storage/ModelTile.cs
@@ -2,6 +2,31 @@
 using System.Collections;
 using System.Collections.Generic;
 
+
+public struct TileKey {
+	public static string COI = "COI";
+	public static string CHIEU = "CHIEU";
+	public static string MIN_VEL = "MIN_VEL";
+	public static string MAX_VEL = "MAX_VEL";
+	public static string RE_TRAI = "RE_TRAI";
+	public static string RE_PHAI = "RE_PHAI";
+	public static string RE_THANG = "RE_THANG";
+	public static string LOAI_XE = "LOAI_XE";
+	public static string DI = "DI_";
+	public static string DUNG = "DUNG_";
+	
+	//Light
+	
+	public static string LIGHT_GROUP_ID = "LIGHT_GROUP_ID";
+	public static string LIGHT_HUONG = "LIGHT_HUONG";
+	public static string LIGHT_LAN_DUONG = "LIGHT_LAN_DUONG";
+	
+	//Sign
+	
+	public static string SIGN_DIR = "SIGN_DIR";
+}
+
+
 public class ModelTile {
 
 	public int objId;
