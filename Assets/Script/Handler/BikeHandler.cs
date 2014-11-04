250ed01f551c6df698f171cfd5cc1f68716883ac
longpham.uit <longpham.uit@gmail.com>
2014-11-04 15:32:43 +0000

    * bike movement!

--- Assets/Script/Handler/BikeHandler.cs
+++ Assets/Script/Handler/BikeHandler.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class BikeHandler : MonoBehaviour {
 
 	public float MaxMoveSpeed = 500;
-	public float RotateSpeed = 3;
+	public float RotateSpeed = 300;
 	public float accelMoveFoward = 100;
 	public float accelMoveBackward = 400;
 	public float accelAutoStop = 100;
@@ -19,21 +19,32 @@ public class BikeHandler : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKey (KeyCode.A) == true || Input.GetKey (KeyCode.LeftArrow) == true) { //left
-			transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y - RotateSpeed, 0);
-		} else if (Input.GetKey (KeyCode.D) == true || Input.GetKey (KeyCode.RightArrow) == true) { //right
-			transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + RotateSpeed, 0);
+		if (Input.GetKey (KeyCode.A) == true 
+		    || Input.GetKey (KeyCode.LeftArrow) == true) { //left
+
+			transform.localEulerAngles = 
+				new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y - RotateSpeed * Time.deltaTime, 0);
+		} else if (Input.GetKey (KeyCode.D) == true 
+		           || Input.GetKey (KeyCode.RightArrow) == true) { //right
+
+			transform.localEulerAngles = 
+				new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + RotateSpeed * Time.deltaTime, 0);
 		}
 
 		//Foward
-		if (Input.GetKey (KeyCode.W) == true || Input.GetKey (KeyCode.UpArrow) == true) { //ahead
+		if (Input.GetKey (KeyCode.W) == true 
+		    || Input.GetKey (KeyCode.UpArrow) == true) { //ahead
+
 			moveSpeed += accelMoveFoward * Time.deltaTime;
 			if (moveSpeed > MaxMoveSpeed) {
 				moveSpeed = MaxMoveSpeed;
 			}
 		}
 
-		if (Input.GetKey (KeyCode.Space) == true) {
+		if (Input.GetKey (KeyCode.Space) == true 
+		    || Input.GetKey (KeyCode.DownArrow) == true 
+		    || Input.GetKey (KeyCode.S) == true) {
+
 			moveSpeed -= accelMoveBackward * Time.deltaTime;
 			if (moveSpeed < 0.2) {
 				moveSpeed = 0;
