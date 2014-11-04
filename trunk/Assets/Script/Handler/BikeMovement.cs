eec58528c4ae1f3e386d330ed634a30cb39e1116
longpham.uit@gmail.com <longpham.uit@gmail.com@f1fda995-d3ca-aaca-87fc-4b9f00a53c2b>
2014-11-04 17:12:54 +0000

    + helmet ON!

---
+++ trunk/Assets/Script/Handler/BikeMovement.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BikeMovement : MonoBehaviour {
+
+	//Moto
+	public float MaxMoveSpeed = 500;
+	public float RotateSpeed = 300;
+	public float accelMoveFoward = 100;
+	public float accelMoveBackward = 400;
+	public float accelAutoStop = 100;
+	private float moveSpeed = 0.0f;
+
+
+	void Start () {}
+
+	void Update () {
+
+		if (Input.GetKey (KeyCode.A) == true 
+		    || Input.GetKey (KeyCode.LeftArrow) == true) { //left
+
+			transform.localEulerAngles = 
+				new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y - RotateSpeed * Time.deltaTime, 0);
+		} else if (Input.GetKey (KeyCode.D) == true 
+		           || Input.GetKey (KeyCode.RightArrow) == true) { //right
+
+			transform.localEulerAngles = 
+				new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y + RotateSpeed * Time.deltaTime, 0);
+		}
+
+		//Foward
+		if (Input.GetKey (KeyCode.W) == true 
+		    || Input.GetKey (KeyCode.UpArrow) == true) { //ahead
+
+			moveSpeed += accelMoveFoward * Time.deltaTime;
+			if (moveSpeed > MaxMoveSpeed) {
+				moveSpeed = MaxMoveSpeed;
+			}
+		}
+
+		if (Input.GetKey (KeyCode.Space) == true 
+		    || Input.GetKey (KeyCode.DownArrow) == true 
+		    || Input.GetKey (KeyCode.S) == true) {
+
+			moveSpeed -= accelMoveBackward * Time.deltaTime;
+			if (moveSpeed < 0.2) {
+				moveSpeed = 0;
+			}
+		}
+
+		transform.localPosition += transform.forward * moveSpeed * Time.deltaTime;
+	}
+}
---
+++ trunk/Assets/Script/Handler/HelmetHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelmetHandler : MonoBehaviour {
+
+	public BikeHandler bikeHandler;
+	private Animator anim;
+
+	void Start () {
+		anim = this.GetComponent<Animator>();
+	}
+
+	void Update () {}
+
+	void OnMouseDown() {
+		bikeHandler.OnHelmetClick ();
+	}
+
+	public void Wear () {
+		anim.SetInteger ("wear", 1);
+	}
+
+	public void UnWear () {
+		anim.SetInteger ("wear", 2);
+	}
+}
