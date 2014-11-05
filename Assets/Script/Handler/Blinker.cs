c4fe99f107aa5bac79683d1e87e09e246fc466f9
longpham.uit <longpham.uit@gmail.com>
2014-11-05 18:15:10 +0000

    + left-right Light!

---
+++ Assets/Script/Handler/Blinker.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blinker : MonoBehaviour {
+
+	public Light targetLight;
+	public bool StartShow = true;
+	public float TimeShow = 1;
+	public float TimeHide = 1;
+
+	private float time = 0;
+
+	void Start () {
+		if (StartShow) {
+			time = 0;
+		} else {
+			time = - TimeHide;
+		}
+	}
+
+	void Update () {
+		time += Time.deltaTime;
+
+		if (time > TimeShow) {
+			time = - TimeHide;
+		}
+
+		if (time > 0) {
+			targetLight.enabled = true;
+		} else if (time < 0) {
+			targetLight.enabled = false;
+		}
+	}
+}
