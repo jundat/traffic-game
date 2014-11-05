b61277fa3161263dfdeae8b50d3e5b9e23312145
longpham.uit@gmail.com <longpham.uit@gmail.com@f1fda995-d3ca-aaca-87fc-4b9f00a53c2b>
2014-11-05 19:03:49 +0000

    + left-right Light!

---
+++ trunk/Assets/Script/Handler/FollowerActive.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowerActive : MonoBehaviour {
+
+	public GameObject target;
+
+	void Start () {}
+
+	void Update () {}
+
+	void OnEnable() {
+		target.SetActive (true);
+	}
+
+	void OnDisable() {
+		target.SetActive (false);
+	}
+}
