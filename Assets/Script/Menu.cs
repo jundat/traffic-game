7df5b6fcff7466b9e1a00f09a0ce2a3b5a474a4a
longpham.uit <longpham.uit@gmail.com>
2014-11-06 15:39:51 +0000

    + Menu v1

---
+++ Assets/Script/Menu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public UIInput inMssv;
+	public UIInput inPassword;
+
+	void Start () {}
+
+	void Update () {}
+
+	public void OnSubmit () {
+		Debug.Log (inMssv.value + ", " + inPassword.value);
+	}
+}
