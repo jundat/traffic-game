dc4b3e49c0dfe66f8c3c671ce8400f9dc90a8140
longpham.uit <longpham.uit@gmail.com>
2014-11-06 17:35:51 +0000

    Menu!

---
+++ Assets/Script/Handler/HistoryItemHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HistoryItemHandler : MonoBehaviour {
+
+	public class Param {
+
+	}
+
+	void Start () {}
+
+	void Update () {}
+
+	public void Init () {
+
+	}
+}
