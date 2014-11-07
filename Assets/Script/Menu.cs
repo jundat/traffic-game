d95d55285edbe0b05baf24757c3cd211f4608af4
longpham.uit <longpham.uit@gmail.com>
2014-11-07 16:23:39 +0000

    + Login + Menu

--- Assets/Script/Menu.cs
+++ Assets/Script/Menu.cs
@@ -4,8 +4,37 @@ using System.Collections;
 public class Menu : MonoBehaviour {
 
 	public GameObject prefabHistoryItem;
+	public UIPanel pnHistory;
+	public UIPanel pnAbout;
 
-	void Start () {}
+	void Start () {
+		pnHistory.gameObject.SetActive (false);
+		pnAbout.gameObject.SetActive (false);
+	}
 
 	void Update () {}
+
+	public void OnPlay () {
+		Application.LoadLevel ("Main");
+	}
+
+	public void OnLogout () {
+		Application.LoadLevel ("Login");
+	}
+	
+	public void OnOpenHistory () {
+		pnHistory.gameObject.SetActive (true);
+	}
+
+	public void OnCloseHistory () {
+		pnHistory.gameObject.SetActive (false);
+	}
+
+	public void OnOpenAbout () {
+		pnAbout.gameObject.SetActive (true);
+	}
+
+	public void OnCloseAbout () {
+		pnAbout.gameObject.SetActive (false);
+	}
 }
