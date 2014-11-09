4d676ffb85f4b86de0675313c5626916a25d6386
longpham.uit@gmail.com <longpham.uit@gmail.com@f1fda995-d3ca-aaca-87fc-4b9f00a53c2b>
2014-11-09 14:56:27 +0000

    + Player State

---
+++ trunk/Assets/Script/Storage/PlayerState.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerState {
+
+	public RoadHandler road;
+
+	public bool isHelmetOn;
+	public bool isLightOn;
+	public bool isNearLight;
+	public int leftRightLight; //-1: left, 0: none, 1: right
+
+	public float speed; //real 0->160 km/h
+
+
+
+	public PlayerState () {}
+
+	public PlayerState Copy () {
+		return new PlayerState ();
+	}
+}
