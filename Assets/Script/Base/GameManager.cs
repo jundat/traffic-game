ff79c21be5c12821e3fcf0d62550117045383fb5
longpham.uit <longpham.uit@gmail.com>
2014-12-06 16:51:37 +0000

    * Refactor: Tile ID

---
+++ Assets/Script/Base/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : SingletonMono<GameManager> {
+
+	public ModelTile startPoint;
+	public ModelTile endPoint;
+	public Dictionary<ModelTile, bool> listCheckpoint = new Dictionary<ModelTile, bool> ();
+
+	void Start () {}
+
+	void Update () {}
+
+	public void AddCheckpoint (ModelTile t) {
+		listCheckpoint[t] = false;
+	}
+}
