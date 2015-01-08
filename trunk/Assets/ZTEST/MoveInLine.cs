aee2461f34939f1a334bb43fd4112f19a2ac03ab
longpham.uit@gmail.com <longpham.uit@gmail.com@f1fda995-d3ca-aaca-87fc-4b9f00a53c2b>
2015-01-08 20:03:45 +0000

    + Auto car Move

---
+++ trunk/Assets/ZTEST/MoveInLine.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveInLine : MonoBehaviour {
+
+	public Transform from;
+	public Transform to;
+	public float v = 0.05f;
+
+	private bool isRun = false;
+	private Vector3 step;
+
+	void Start () {}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			isRun = true;
+			transform.LookAt (to);
+			step = (to.position - from.position) / Vector3.Distance (from.position, to.position);
+		}
+
+		if (isRun) {
+			Vector3 move = step * v;
+			transform.position = transform.position + move;
+
+			if (Vector3.Distance (transform.position, to.position) < 1) {
+				Debug.Log ("OK");
+				transform.position = to.position;
+				isRun = false;
+			}
+		}
+	}
+}
